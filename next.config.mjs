129e173b43f989f5d1cb96b354ef4a790842da60
its-Danial <itsmalikdanial@gmail.com>
2024-06-02 17:59:39 +0500

    added image hostname

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fedskillstest.ct.digital",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
