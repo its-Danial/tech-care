b068e2f88a4955aab4d2a5824734c4820021e7c6
its-Danial <itsmalikdanial@gmail.com>
2024-06-02 16:48:58 +0500

    change font class

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -44,19 +44,12 @@ export default function Nav() {
           <div
             key={link.label}
             className={clsx(
-              "flex h-full cursor-pointer items-center space-x-[17px]",
+              "flex h-full cursor-pointer items-center space-x-2",
               link.active && styles.active,
             )}
           >
             <Image src={link.icon} alt={link.label} width="17" height="16" />
-            <span
-              className={clsx(
-                styles["link-label"],
-                "text-sm font-semibold leading-5 text-[var(--unnamed-color-072635)]",
-              )}
-            >
-              {link.label}
-            </span>
+            <span className="body-emphasized-14pt">{link.label}</span>
           </div>
         ))}
       </div>
@@ -69,10 +62,8 @@ export default function Nav() {
         />
         <div className="item flex h-full w-full flex-1 justify-between">
           <div className="flex h-full flex-1 flex-col justify-center text-xs">
-            <span className="text-[var(--unnamed-color-072635] font-semibold">
-              Dr. Jose Simmons
-            </span>
-            <span className="text-[var(--unnamed-color-707070)]">
+            <span className="body-emphasized-14pt">Dr. Jose Simmons</span>
+            <span className="body-secondary-info-14pt">
               General Practitioner
             </span>
           </div>
